a3acaf728326126dbda13e75aad3a2579e4dae42
Richard Germaine <rlgerma92@outlook.com>
2020-07-10 15:03:01 -0600

    directory maint

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,4 +1,5 @@
 import firebase from "firebase/app";
+import dotenv from "dotenv";
 import { navigate } from "@reach/router";
 import "firebase/auth";
 import "firebase/firestore";
@@ -7,7 +8,7 @@ export const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
+  projectId: "bootspacetwo",
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
