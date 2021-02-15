2017eb6183a8c210bd2c1874f403fd565bd1e84e
Richard Germaine <richard.germaine@icloud.com>
2021-02-15 13:56:23 -0700

    *explosive noises

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-import { join } from "path";
-const router = require("express").Router();
-
-router.use(function (res) {
-  res.sendFile(join(__dirname, "../client/build/index.html"));
-});
-
-export default router;
